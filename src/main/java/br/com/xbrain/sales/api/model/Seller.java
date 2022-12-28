2112d6636da579b7426219cccb36da73af56841a
Robson Barbosa Souza <robson.souza@olgari.com.br>
2022-12-28 13:17:00 -0300

    chore: add entity notation for persistence of Seller model

--- src/main/java/br/com/xbrain/sales/api/model/Seller.java
+++ src/main/java/br/com/xbrain/sales/api/model/Seller.java
@@ -1,6 +1,20 @@
 package br.com.xbrain.sales.api.model;
 
-public class Seller  {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
+public class Seller {
+
+  @EqualsAndHashCode.Include
+  @Id
   private Long id;
-  private String nome;
+  private String name;
 }
