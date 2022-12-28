9e4103885f4f1c33cdca1573a82de013fc5dd210
Robson Barbosa Souza <robson.souza@olgari.com.br>
2022-12-28 13:17:33 -0300

    chore: add entity notation for persistence of Sale model

--- src/main/java/br/com/xbrain/sales/api/model/Sale.java
+++ src/main/java/br/com/xbrain/sales/api/model/Sale.java
@@ -2,9 +2,37 @@ package br.com.xbrain.sales.api.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
 public class Sale {
+
+  @EqualsAndHashCode.Include
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @NotBlank
+  @Min(1)
+  @Column(name = "value_in_cents")
   private Long value;
+  @NotBlank
+  @NotNull
+  @ManyToOne
   private Seller seller;
   private LocalDateTime createdAt;
 }
