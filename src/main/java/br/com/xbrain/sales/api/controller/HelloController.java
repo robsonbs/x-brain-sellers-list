32dc9afcc21c6b084ebbe59ff7ca51c4b6078497
Robson Barbosa Souza <robson.souza@olgari.com.br>
2022-12-28 09:25:06 -0300

    chore: make controller welcome to test project

---
+++ src/main/java/br/com/xbrain/sales/api/controller/HelloController.java
@@ -0,0 +1,13 @@
+package br.com.xbrain.sales.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public String index() {
+    return "Welcome to rest api";
+  }
+}
