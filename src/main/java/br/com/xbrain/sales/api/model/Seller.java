dc42d3c14ca24d77c1330f2ff5dc445cc456e1ce
Robson Barbosa Souza <robson.souza@olgari.com.br>
2022-12-28 15:58:32 -0300

    chore: improve rules of size of seller name

--- src/main/java/br/com/xbrain/sales/api/model/Seller.java
+++ src/main/java/br/com/xbrain/sales/api/model/Seller.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,5 +21,7 @@ public class Seller {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @NotBlank
+  @Size(min = 3, max = 128)
   private String name;
 }
