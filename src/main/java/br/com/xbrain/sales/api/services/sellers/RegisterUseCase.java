e9f6441d7b161772268f0b81e46f2ef1f4407ba2
Robson Barbosa Souza <robson.souza@olgari.com.br>
2022-12-28 21:54:04 -0300

    feat: create seller by service

---
+++ src/main/java/br/com/xbrain/sales/api/services/sellers/RegisterUseCase.java
@@ -0,0 +1,17 @@
+package br.com.xbrain.sales.api.services.sellers;
+
+import br.com.xbrain.sales.api.model.Seller;
+import br.com.xbrain.sales.api.repository.SellerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RegisterUseCase {
+
+  private final SellerRepository sellerRepository;
+
+  public Seller registerSeller(Seller seller) {
+    return sellerRepository.save(seller);
+  }
+}
