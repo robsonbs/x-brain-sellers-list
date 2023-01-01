9ca7c3b3ea1ff1deb6667816f34d76df1fcf9c54
Robson Barbosa Souza <robson.souza@olgari.com.br>
2023-01-01 09:49:23 -0300

    test: testNotSaveSaleWithoutSeller change to improve

--- src/test/java/br/com/xbrain/sales/api/service/sales/RegisterSaleUseCaseTest.java
+++ src/test/java/br/com/xbrain/sales/api/service/sales/RegisterSaleUseCaseTest.java
@@ -1,14 +1,16 @@
 package br.com.xbrain.sales.api.service.sales;
 
+import br.com.xbrain.sales.api.exceptions.BusinessException;
 import br.com.xbrain.sales.api.model.Sale;
 import br.com.xbrain.sales.api.repository.SaleRepository;
 import br.com.xbrain.sales.api.services.sales.RegisterSaleUseCase;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -29,7 +31,8 @@ public class RegisterSaleUseCaseTest {
   @Test
   void testNotSaveSaleWithoutSeller() {
     Sale sale = new Sale();
-    Sale savedSale = registerSaleUseCase.registerSale(sale);
-    Assertions.assertThat(savedSale).isNull();
+    BusinessException error =
+      assertThrows(BusinessException.class, () -> registerSaleUseCase.registerSale(sale));
+    assertEquals("Seller Not Valid!", error.getMessage());
   }
 }
